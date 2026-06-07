d74bb571a6deb8b5f044789d1f878824dc42b57b
barsa-dot <UGCET2502471@reva.edu.in>
2026-06-07 21:55:14 +0530

    Added circle option

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+
+#define ROWS 30
+#define COLS 80
+
+#define RECTANGLE 1
+#define LINE 2
+#define TRIANGLE 3
+#define CIRCLE 4
 
 typedef struct
 {
@@ -22,142 +31,169 @@ typedef struct
 
 } Shape;
 
-#define ROWS 30
-#define COLS 80
-
-#define RECTANGLE 1
-#define LINE 2
-#define TRIANGLE 3
-#define CIRCLE 4
-
 char canvas[ROWS][COLS];
 
 Shape shapes[100];
 
 int count = 0;
 
+/* ==========================
+   Canvas Functions
+   ========================== */
+
 void initCanvas()
 {
-    int i,j;
+    int i, j;
 
-    for(i=0;i<ROWS;i++)
+    for(i = 0; i < ROWS; i++)
     {
-        for(j=0;j<COLS;j++)
+        for(j = 0; j < COLS; j++)
         {
-            canvas[i][j]=' ';
+            canvas[i][j] = ' ';
         }
     }
 }
 
 void displayCanvas()
 {
-    int i,j;
+    int i, j;
 
-    for(i=0;i<ROWS;i++)
+    for(i = 0; i < ROWS; i++)
     {
-        for(j=0;j<COLS;j++)
+        for(j = 0; j < COLS; j++)
         {
-            printf("%c",canvas[i][j]);
+            printf("%c", canvas[i][j]);
         }
 
         printf("\n");
     }
 }
 
-void drawRectangle(int x,int y,int width,int height)
+/* ==========================
+   Drawing Functions
+   ========================== */
+
+void drawRectangle(int x, int y, int width, int height)
 {
     int i;
 
-    for(i=0;i<width;i++)
+    for(i = 0; i < width; i++)
     {
-        canvas[x][y+i]='*';
-        canvas[x+height-1][y+i]='*';
+        canvas[x][y + i] = '*';
+        canvas[x + height - 1][y + i] = '*';
     }
 
-    for(i=0;i<height;i++)
+    for(i = 0; i < height; i++)
     {
-        canvas[x+i][y]='*';
-        canvas[x+i][y+width-1]='*';
+        canvas[x + i][y] = '*';
+        canvas[x + i][y + width - 1] = '*';
     }
 }
 
-void drawLine(int x1,int y1,int x2,int y2)
+void drawLine(int x1, int y1, int x2, int y2)
 {
     int i;
 
-    if(x1 == x2) // horizontal line
+    if(x1 == x2)
     {
-        for(i=y1;i<=y2;i++)
+        for(i = y1; i <= y2; i++)
         {
-            canvas[x1][i]='*';
+            canvas[x1][i] = '*';
         }
     }
-    else if(y1 == y2) // vertical line
+    else if(y1 == y2)
     {
-        for(i=x1;i<=x2;i++)
+        for(i = x1; i <= x2; i++)
         {
-            canvas[i][y1]='*';
+            canvas[i][y1] = '*';
         }
     }
 }
 
-void drawTriangle(int x,int y,int size)
+void drawTriangle(int x, int y, int size)
 {
     int i;
 
-    for(i=0;i<size;i++)
+    for(i = 0; i < size; i++)
     {
-        canvas[x+i][y]='*';
+        canvas[x + i][y] = '*';
 
-        canvas[x+size-1][y+i]='*';
+        canvas[x + size - 1][y + i] = '*';
 
-        canvas[x+i][y+i]='*';
+        canvas[x + i][y + i] = '*';
     }
 }
 
-void drawCircle(int cx,int cy,int r)
+void drawCircle(int cx, int cy, int r)
 {
-    int x,y;
+    int x, y;
 
-    for(x=0;x<ROWS;x++)
+    for(x = 0; x < ROWS; x++)
     {
-        for(y=0;y<COLS;y++)
+        for(y = 0; y < COLS; y++)
         {
             int value =
-                (x-cx)*(x-cx) +
-                (y-cy)*(y-cy);
+                (x - cx) * (x - cx) +
+                (y - cy) * (y - cy);
 
-            if(abs(value - r*r) <= r)
+            if(abs(value - r * r) <= r)
             {
-                canvas[x][y]='*';
+                canvas[x][y] = '*';
             }
         }
     }
 }
+
+/* ==========================
+   Add Shape Functions
+   ========================== */
 
 void addRectangle()
 {
     Shape s;
 
     printf("Row: ");
-    scanf("%d",&s.x);
+    scanf("%d", &s.x);
 
     printf("Column: ");
-    scanf("%d",&s.y);
+    scanf("%d", &s.y);
 
     printf("Width: ");
-    scanf("%d",&s.width);
+    scanf("%d", &s.width);
 
     printf("Height: ");
-    scanf("%d",&s.height);
+    scanf("%d", &s.height);
 
     s.type = RECTANGLE;
     s.active = 1;
 
     shapes[count] = s;
-
     count++;
 }
+
+void addCircle()
+{
+    Shape s;
+
+    printf("Center Row: ");
+    scanf("%d", &s.x);
+
+    printf("Center Column: ");
+    scanf("%d", &s.y);
+
+    printf("Radius: ");
+    scanf("%d", &s.radius);
+
+    s.type = CIRCLE;
+    s.active = 1;
+
+    shapes[count] = s;
+    count++;
+}
+
+/* ==========================
+   Redraw All Shapes
+   ========================== */
 
 void redrawAllShapes()
 {
@@ -165,7 +201,7 @@ void redrawAllShapes()
 
     initCanvas();
 
-    for(i=0;i<count;i++)
+    for(i = 0; i < count; i++)
     {
         if(shapes[i].active)
         {
@@ -178,22 +214,39 @@ void redrawAllShapes()
                     shapes[i].height
                 );
             }
+            else if(shapes[i].type == CIRCLE)
+            {
+                drawCircle(
+                    shapes[i].x,
+                    shapes[i].y,
+                    shapes[i].radius
+                );
+            }
         }
     }
 }
+
+/* ==========================
+   Main Program
+   ========================== */
 
 int main()
 {
     int choice;
 
+    initCanvas();
+
     while(1)
     {
         printf("\n");
+        printf("===== 2D Graphics Editor =====\n");
         printf("1. Add Rectangle\n");
-        printf("2. Display\n");
-        printf("3. Exit\n");
+        printf("2. Add Circle\n");
+        printf("3. Display Canvas\n");
+        printf("4. Exit\n");
+        printf("Enter choice: ");
 
-        scanf("%d",&choice);
+        scanf("%d", &choice);
 
         switch(choice)
         {
@@ -203,11 +256,22 @@ int main()
                 break;
 
             case 2:
-                displayCanvas();
+                addCircle();
+                redrawAllShapes();
                 break;
 
             case 3:
+                displayCanvas();
+                break;
+
+            case 4:
+                printf("Exiting...\n");
                 return 0;
+
+            default:
+                printf("Invalid choice!\n");
         }
     }
+
+    return 0;
 }
